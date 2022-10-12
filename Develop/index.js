ce20e424066f316ced9b20bf99e062862401b4fc
Allison <farrella95@gmail.com>
2022-10-12 19:45:05 -0400

    ooga booga

--- Develop/index.js
+++ Develop/index.js
@@ -3,56 +3,53 @@ const generateMkdwn = require("./utils/generateMarkdown");
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
-  {
-    type: "input",
-    message: "Description of your project",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "Provide Table of Contents",
-    name: "table of contents",
-  },
-  {
-    type: "input",
-    message: "Provide installation instructions",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "Provide usage of project",
-    name: "usage",
-  },
-  {
-    type: "list",
-    message: "Provide license type",
-    name: "license",
-    choices: ["MIT", "GPL", "GNU", "LGPL", "Apache", "BSD"],
-  },
-  {
-    type: "input",
-    message: "Any contributors?",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "Provide test instructions",
-    name: "tests",
-  },
-  {
-    type: "input",
-    message: "Questions?",
-    name: "questions",
-  },
-]);
-//   .then((data) => {
-//     const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
-
-//     fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
-//       err ? console.log(err) : console.log("Success!")
-//     );
-//   });
+const questions = inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Description of your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Provide Table of Contents",
+      name: "table of contents",
+    },
+    {
+      type: "input",
+      message: "Provide installation instructions",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Provide usage of project",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "Provide license type",
+      name: "license",
+      choices: ["MIT", "GPL", "GNU", "LGPL", "Apache", "BSD"],
+    },
+    {
+      type: "input",
+      message: "Any contributors?",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Provide test instructions",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Questions?",
+      name: "questions",
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
