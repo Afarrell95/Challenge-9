12aae1467b906fdcfac80dbce3f71260decdf28b
Allison <farrella95@gmail.com>
2022-10-12 17:41:26 -0400

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,33 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = require("./generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username",
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password",
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm",
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
