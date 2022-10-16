bd6082cfac762b712a7e6a2e0f391ec6ccb1b297
Allison <farrella95@gmail.com>
2022-10-16 11:42:15 -0400

    AHHHHHHHHHHH

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,74 @@
 // TODO: Include packages needed for this application
-const generateMkdwn = require("./utils/generateMarkdown");
+// const generateMkdwn = require("./utils/generateMarkdown.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const makeReadMe = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const prompt = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Description of your project",
-        name: "description",
-      },
-      {
-        type: "input",
-        message: "Provide Table of Contents",
-        name: "table of contents",
-      },
-      {
-        type: "input",
-        message: "Provide installation instructions",
-        name: "installation",
-      },
-      {
-        type: "input",
-        message: "Provide usage of project",
-        name: "usage",
-      },
-      {
-        type: "list",
-        message: "Provide license type",
-        name: "license",
-        choices: ["MIT", "GPL", "GNU", "LGPL", "Apache", "BSD"],
-      },
-      {
-        type: "input",
-        message: "Any contributors?",
-        name: "contributing",
-      },
-      {
-        type: "input",
-        message: "Provide test instructions",
-        name: "tests",
-      },
-      {
-        type: "input",
-        message: "Questions?",
-        name: "questions",
-      },
-    ])
-    .then((data) => {
-      console.log(data);
-      // return data;
+const questions = [
+  {
+    type: "input",
+    message: "Title",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Description of your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Provide Table of Contents",
+    name: "table of contents",
+  },
+  {
+    type: "input",
+    message: "Provide installation instructions",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide usage of project",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Provide license type",
+    name: "license",
+    choices: ["MIT", "GPL", "GNU", "LGPL", "Apache", "BSD"],
+  },
+  {
+    type: "input",
+    message: "Any contributors?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Provide test instructions",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Questions?",
+    name: "questions",
+  },
+];
+// .then((data) => {
+//   console.log(data);
+//   // return data;
 
-      // TODO: Create a function to write README file
-      // function writeToFile(data) {
-      const readmeContent = generateMarkdown(data);
+//   // TODO: Create a function to write README file
+// function writeToFile(data) {
 
-      fs.writeFile("README.md", readmeContent, (err) =>
-        err ? console.log(err) : console.log("Successfully created README!")
-      );
-      // }
-    });
-};
 // TODO: Create a function to initialize app
 const init = () => {
-  prompt()
-    .then(() => writeToFile())
-    .then(() => console.log("Successfully created README!"))
-    .catch((err) => console.error(err));
+  return inquirer.prompt(questions).then((data) => {
+    const readmeContent = makeReadMe.generateMarkdown(data);
+    console.log(readmeContent);
+    return data;
+  });
+  // .then((data) => fs.writeFile("README.md", readmeContent, (err) =>
+  // err ? console.log(err) : console.log("Successfully created README!")
+  // )
 };
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,70 +1,61 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(data) {
-  const licenseType = data.license;
-  let badge;
-  if (licenseType === "MIT") {
-    badge =
-      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else if (licenseType === "GPL") {
-    badge =
-      "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
-  } else if (licenseType === "GNU") {
-    badge =
-      "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
-  } else if (licenseType === "LGPL") {
-    badge =
-      "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-  } else if (licenseType === "Apache") {
-    badge =
-      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  } else if (licenseType === "BSD") {
-    badge =
-      "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
-  } else {
-    badge = "";
-  }
-}
+class makeReadMe {
+  // TODO: Create a function that returns a license badge based on which license is passed in
+  // If there is no license, return an empty string
+  // renderLicenseBadge(data) {
+  //   const licenseType = data.license;
+  //   let badge;
+  //   if (licenseType === "MIT") {
+  //     badge =
+  //       "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  //   } else if (licenseType === "GPL") {
+  //     badge =
+  //       "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+  //   } else if (licenseType === "GNU") {
+  //     badge =
+  //       "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
+  //   } else if (licenseType === "LGPL") {
+  //     badge =
+  //       "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+  //   } else if (licenseType === "Apache") {
+  //     badge =
+  //       "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  //   } else if (licenseType === "BSD") {
+  //     badge =
+  //       "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+  //   } else {
+  //     badge = "";
+  //   }
+  // }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(data) {
-  if (license === "MIT") {
-  } else if (license === "GPL") {
-  } else if (license === "GNU") {
-  } else if (license === "LGPL") {
-  } else if (license === "Apache") {
-  } else if (license === "BSD") {
-  } else {
-    return "";
-  }
-}
+  // // TODO: Create a function that returns the license link
+  // // If there is no license, return an empty string
+  // renderLicenseLink(data) {
+  //   if (license === "MIT") {
+  //   } else if (license === "GPL") {
+  //   } else if (license === "GNU") {
+  //   } else if (license === "LGPL") {
+  //   } else if (license === "Apache") {
+  //   } else if (license === "BSD") {
+  //   } else {
+  //     return "";
+  //   }
+  // }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(data) {
-  if (license === "MIT") {
-  } else if (license === "GPL") {
-  } else if (license === "GNU") {
-  } else if (license === "LGPL") {
-  } else if (license === "Apache") {
-  } else if (license === "BSD") {
-  } else {
-    return "";
-  }
-}
+  // // TODO: Create a function that returns the license section of README
+  // // If there is no license, return an empty string
+  // renderLicenseSection(data) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  // TODO: Create a function to generate markdown for README
+  static generateMarkdown(data) {
+    return `# ${data.title}
   ## Table of Contents
-  -(#description)
-  -(#installation)
-  -(#usage)
-  -(#license)
-  -(#contributing)
-  -(#tests)
-  -(#questions)
+  -[Description](#description)
+  -[Install](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributions](#contributing)
+  -[Testing](#tests)
+  -[Questions](#questions)
 
   ## description
   ${data.description}
@@ -73,7 +64,7 @@ function generateMarkdown(data) {
   ## usage
   ${data.usage}
   ## license
-  ${renderLicenseBadge(data)}
+  ${data.license}
   ## contributing
   ${data.contributing}
   ## tests
@@ -81,6 +72,7 @@ function generateMarkdown(data) {
   ## questions
   ${data.questions}
 `;
+  }
 }
 
-module.exports = "/generateMarkdown";
+module.exports = makeReadMe;
