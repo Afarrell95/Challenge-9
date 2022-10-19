1b3b97f4e5a6a06946d3d670076854d4672f9553
Allison <farrella95@gmail.com>
2022-10-19 09:51:17 -0400

    finish

---
+++ Develop/README.md
@@ -0,0 +1,26 @@
+# challenge 9
+  ## Table of Contents
+  -[Description](#description)
+  -[Install](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributions](#contributing)
+  -[Testing](#tests)
+  -[Questions](#questions)
+
+  ## description
+  an app created to generate the user a readme document with node.js and the inquirer npm package
+  ## installation
+  n/a
+  ## usage
+  generate readme document
+  ## license
+  MIT
+  
+  ## contributing
+  n/a
+  ## tests
+  n/a
+  ## questions
+  farrella95
+  farrella95@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,19 @@
 class makeReadMe {
   // TODO: Create a function that returns a license badge based on which license is passed in
   // If there is no license, return an empty string
-  renderLicenseBadge(data) {
-    const licenseType = data.license;
-    if (licenseType === "MIT") {
-      return;
-      ("[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)");
-    } else if (licenseType === "GPL") {
-      return;
-      ("[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)");
-    } else if (licenseType === "GNU") {
-      return;
-      ("[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)");
-    } else if (licenseType === "LGPL") {
-      return;
-      ("[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)");
-    } else if (licenseType === "Apache") {
-      return;
-      ("[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
-    } else if (licenseType === "BSD") {
-      return;
-      ("[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)");
+  renderLicenseBadge(license) {
+    if (license === "MIT") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license === "GPL") {
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+    } else if (license === "GNU") {
+      return "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
+    } else if (license === "LGPL") {
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+    } else if (license === "Apache") {
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (license === "BSD") {
+      return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
     } else {
       return "";
     }
@@ -64,6 +57,7 @@ class makeReadMe {
   ${data.usage}
   ## license
   ${data.license}
+  
   ## contributing
   ${data.contributing}
   ## tests
